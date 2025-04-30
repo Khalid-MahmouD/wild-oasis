ed776e64c5d1921ede6aa525c7f77a3f6353cece
Khalid-MahmouD <khaloooood827@gmail.com>
2025-04-30 11:38:02 +0300

    app layout

---
+++ src/ui/AppLayout.jsx
@@ -0,0 +1,30 @@
+import { Outlet } from "react-router-dom";
+import Header from "../ui/Header";
+import Sidebar from "./Sidebar";
+import styled from "styled-components";
+
+const Main = styled.main`
+  background-color: var(--color-gray-50);
+  padding: 4rem 4.8rem 6.4rem;
+`;
+
+const StyledLayout = styled.div`
+  display: grid;
+  grid-template-columns: 26rem 1fr;
+  grid-template-rows: auto 1fr;
+  height: 100vh;
+`;
+
+function AppLayout() {
+  return (
+    <StyledLayout>
+      <Header />
+      <Sidebar />
+      <Main>
+        <Outlet />
+      </Main>
+    </StyledLayout>
+  );
+}
+
+export default AppLayout;

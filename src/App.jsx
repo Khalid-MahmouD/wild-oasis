ed4813ace7c42ebcc686c8cb612bf8fce2c3bd55
Khalid-MahmouD <khaloooood827@gmail.com>
2025-05-01 10:37:45 +0300

    navigation part

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 
 import GlobalStyles from "./styles/GlobalStyles";
 import Dashboard from "./pages/Dashboard";
+import Bookings from "./pages/Bookings";
 import Cabins from "./pages/Cabins";
 import Users from "./pages/Users";
 import Settings from "./pages/Settings";
@@ -9,6 +10,7 @@ import Account from "./pages/Account";
 import Login from "./pages/Login";
 import PageNotFound from "./pages/PageNotFound";
 import AppLayout from "./ui/AppLayout";
+
 function App() {
   return (
     <>
@@ -18,6 +20,7 @@ function App() {
           <Route element={<AppLayout />}>
             <Route index element={<Navigate replace to="dashboard" />} />
             <Route path="dashboard" element={<Dashboard />} />
+            <Route path="bookings" element={<Bookings />} />
             <Route path="cabins" element={<Cabins />} />
             <Route path="users" element={<Users />} />
             <Route path="settings" element={<Settings />} />
--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -2,9 +2,8 @@ import { Outlet } from "react-router-dom";
 import Header from "../ui/Header";
 import Sidebar from "./Sidebar";
 import styled from "styled-components";
-
 const Main = styled.main`
-  background-color: var(--color-gray-50);
+  background-color: var(--color-grey-50);
   padding: 4rem 4.8rem 6.4rem;
 `;
 

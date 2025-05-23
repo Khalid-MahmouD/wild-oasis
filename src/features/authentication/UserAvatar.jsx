478c7e78fe1e42290b2fe8504f45e4e7b25d07e7
Khalid-MahmouD <40337849+Khalid-MahmouD@users.noreply.github.com>
2025-05-23 19:31:44 +0300

    Add UserAvatar and HeaderMenu components; integrate UserAvatar into Header for user display

--- src/features/authentication/UserAvatar.jsx
+++ src/features/authentication/UserAvatar.jsx
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import { useUser } from "./useUser";
 
 const StyledUserAvatar = styled.div`
   display: flex;
@@ -19,3 +20,15 @@ const Avatar = styled.img`
   border-radius: 50%;
   outline: 2px solid var(--color-grey-100);
 `;
+
+function UserAvatar() {
+  const { user } = useUser();
+
+  return (
+    <div>
+      {user?.user_metadata ? `Avatar: ${user.user_metadata.name}` : "Avatar"}
+    </div>
+  )
+}
+
+export default UserAvatar
